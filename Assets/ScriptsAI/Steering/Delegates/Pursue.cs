184639eeaa2d782f254b3a250027dfefed322af9
Shiroi Max <maximelkontestano@gmail.com>
2023-02-14 18:11:22 +0100

    Pursue

---
+++ Assets/ScriptsAI/Steering/Delegates/Pursue.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursue : SteeringBehaviour
+{
+    public Seek seek;
+    private Agent npcvtemp;
+    public Agent npcv;
+    public float maxPrediction = 100;
+
+
+    void Start()
+    {
+
+    }
+
+    public override Steering GetSteering(Agent agent)
+    {
+        Vector3 direction = target.Position - agent.Position;
+        float distance = direction.magnitude;
+        float speed = agent.Velocity.magnitude;
+
+        float prediction;
+        if (speed <= distance / maxPrediction)
+            prediction = maxPrediction;
+        else
+            prediction = distance / speed;
+
+        if (npcvtemp == null)
+            npcvtemp = Instantiate(npcv);
+        npcvtemp.Position = target.Position;
+        npcvtemp.Position += target.Velocity * prediction;
+        seek.target = npcvtemp;
+
+        return seek.GetSteering(agent);
+    }
+}
